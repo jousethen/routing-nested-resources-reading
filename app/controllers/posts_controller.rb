71081252c3c103468d54dfa8975d4917386ad616
jousethen <jthen93@gmail.com>
2021-07-07 23:34:05 -0400

    commit tree 9909e3505a5ba2e12dcfcdaf7bcb7d1319f6d44b

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,9 +11,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end 
-  def show
-    @post = Post.find(params[:id])
-  end
+
 
   def new
     @post = Post.new
